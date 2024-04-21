f3652b0b6e2ae845e1d1d0ea8b56a1f5487845a3
TheJustRusik <7thejustrusik7@gmail.com>
2024-04-21 22:27:24 +0500

    New table💊

--- src/main/resources/db/migration/V1.0__init.sql
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -1,5 +1,5 @@
 --Users and roles
-create table users (
+create table if not exists users (
     id              bigserial primary key,
     nickname        varchar(128) unique not null,
     first_name      varchar(64) not null,
@@ -9,18 +9,30 @@ create table users (
     created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP
 );
 
-create table roles (
+create table if not exists roles (
     id              serial primary key,
     role            varchar(16) not null unique
 );
 
-create table user_roles (
+create table if not exists user_roles (
     role_id         int references roles,
     user_id         bigint references users
 );
 
+create table if not exists relations (
+    id              serial primary key,
+    name            varchar(16) not null unique
+);
+
+create table if not exists relationship (
+    initiator       bigint references users,
+    victim          bigint references users,
+    relation_type   int references relations,
+    created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP
+);
+
 --Messages
-create table messages (
+create table if not exists messages (
     id              bigserial primary key,
     sender          bigint not null references users,
     recipient       bigint not null references users,
@@ -28,14 +40,14 @@ create table messages (
     created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP
 );
 
-create table message_reactions (
+create table if not exists message_reactions (
     message_id      int references messages,
     sender_id       bigint references users,
     reaction        varchar(8) not null
 );
 
 --Posts, likes, comments
-create table posts (
+create table if not exists posts (
     id              bigserial primary key,
     author          bigint not null references users,
     text_content    text,
@@ -44,14 +56,14 @@ create table posts (
     comments        bigint default 0
 );
 
-create table post_likes (
+create table if not exists post_likes (
     user_id         bigint references users,
     post_id         bigint references posts,
     created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP,
     primary key (user_id, post_id)
 );
 
-create table comments (
+create table if not exists comments (
     id              bigserial primary key,
     author          bigint references users not null,
     post_id         bigint references posts,
@@ -60,10 +72,12 @@ create table comments (
     created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP
 );
 
-create table comment_likes (
+create table if not exists comment_likes (
     user_id         bigint references users,
     comment_id      bigint references comments,
     created_time    timestamp not null DEFAULT CURRENT_TIMESTAMP,
     primary key (user_id, comment_id)
 );
 
+insert into users values
+                      (1, 'TheJustRusik', 'Ruslan', '', '7thejustrusik9@gmail.com', '$2a$05$4yj17z9Ciges6y8F/C1PQuTmsqeW2/qoG0EDIPu/O4nThLWrHKes6', CURRENT_TIMESTAMP);
