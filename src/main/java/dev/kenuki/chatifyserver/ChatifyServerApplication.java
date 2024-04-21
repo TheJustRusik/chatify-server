17f22095d01f5cfd7007bfa7f94ca7b3dfcd6169
TheJustRusik <7thejustrusik7@gmail.com>
2024-04-21 17:39:34 +0500

    Initial commit

---
+++ src/main/java/dev/kenuki/chatifyserver/ChatifyServerApplication.java
@@ -0,0 +1,13 @@
+package dev.kenuki.chatifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatifyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kenuki/chatifyserver/ChatifyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kenuki.chatifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatifyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
